3446cae61f45094bbaa625e23f8d3f9daa026f20
Ahmed Talaat <ahmedtm2015@gmail.com>
2022-10-27 23:41:16 +0300

    adding more costms.🤔

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,15 +36,25 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Accio));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.commandToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.varoiusInventoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.accioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.commandToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newRFMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newInvoiceForRFMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -59,15 +69,35 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 377F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(828, 377);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Info;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(168, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(657, 371);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.tableLayoutPanel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(159, 371);
+            this.panel1.TabIndex = 0;
+            // 
             // menuStrip1
             // 
-            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.commandToolStripMenuItem,
+            this.commandToolStripMenuItem1,
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
@@ -80,8 +110,14 @@
             this.commandToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.varoiusInventoriesToolStripMenuItem});
             this.commandToolStripMenuItem.Name = "commandToolStripMenuItem";
-            this.commandToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
-            this.commandToolStripMenuItem.Text = "Command";
+            this.commandToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.commandToolStripMenuItem.Text = "Lookup";
+            // 
+            // varoiusInventoriesToolStripMenuItem
+            // 
+            this.varoiusInventoriesToolStripMenuItem.Name = "varoiusInventoriesToolStripMenuItem";
+            this.varoiusInventoriesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.varoiusInventoriesToolStripMenuItem.Text = "Varoius Inventories";
             // 
             // aboutToolStripMenuItem
             // 
@@ -91,40 +127,105 @@
             this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
             this.aboutToolStripMenuItem.Text = "About";
             // 
-            // varoiusInventoriesToolStripMenuItem
-            // 
-            this.varoiusInventoriesToolStripMenuItem.Name = "varoiusInventoriesToolStripMenuItem";
-            this.varoiusInventoriesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.varoiusInventoriesToolStripMenuItem.Text = "Varoius Inventories";
-            // 
             // accioToolStripMenuItem
             // 
             this.accioToolStripMenuItem.Name = "accioToolStripMenuItem";
             this.accioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.accioToolStripMenuItem.Text = "Accio";
             // 
-            // panel1
+            // commandToolStripMenuItem1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(159, 371);
-            this.panel1.TabIndex = 0;
+            this.commandToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newRFMToolStripMenuItem,
+            this.newInvoiceForRFMToolStripMenuItem});
+            this.commandToolStripMenuItem1.Name = "commandToolStripMenuItem1";
+            this.commandToolStripMenuItem1.Size = new System.Drawing.Size(76, 20);
+            this.commandToolStripMenuItem1.Text = "Command";
             // 
-            // panel2
+            // newRFMToolStripMenuItem
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(168, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(657, 371);
-            this.panel2.TabIndex = 1;
+            this.newRFMToolStripMenuItem.Name = "newRFMToolStripMenuItem";
+            this.newRFMToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.newRFMToolStripMenuItem.Text = "New RFM";
+            // 
+            // newInvoiceForRFMToolStripMenuItem
+            // 
+            this.newInvoiceForRFMToolStripMenuItem.Name = "newInvoiceForRFMToolStripMenuItem";
+            this.newInvoiceForRFMToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.newInvoiceForRFMToolStripMenuItem.Text = "New Invoice for RFM";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.button4, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 8;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(159, 371);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cards";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(3, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Adminstration";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(3, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Configs";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(3, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Logout";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Accio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(852, 420);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.menuStrip1);
@@ -134,8 +235,10 @@
             this.Text = "Accio";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,6 +254,14 @@
         private System.Windows.Forms.ToolStripMenuItem accioToolStripMenuItem;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem commandToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newRFMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newInvoiceForRFMToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,13 +17,49 @@ namespace AccioInventory
         {
             InitializeComponent();
 
+            this.Controls.Add(SetMyFooter());
+
+
             this.Resize += (s, e) => {
 
                 this.tableLayoutPanel1.Location = new Point(3, 27);
-                this.tableLayoutPanel1.Size = new System.Drawing.Size(this.Size.Width-26, this.Size.Height-86);
+                this.tableLayoutPanel1.Size = new System.Drawing.Size(this.Size.Width-22, this.Size.Height-84);
 
 
             };
+
+           
         }
+
+        private Control SetMyFooter()
+        {
+            Panel footParent = new Panel { BackColor = Color.LightGray, Size = new Size(this.Width, 60), Location = new Point(0, this.Height - 60) };
+            this.Resize += (s, e) => {
+                footParent.Location = new Point(0, this.Height - 60);
+                footParent.Size = new Size(this.Width, 60);
+            };
+            Label inf = new Label { Size = footParent.Size, Location = new Point(5,4), Text = "This project mainly developed by Eng.Ahmed T -- Haam Limited Co. 2022." };
+            footParent.Controls.Add(inf);
+                return footParent;
+        }
+
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            CloseCancel(e);
+        }
+
+        public static void CloseCancel(FormClosingEventArgs e)
+        {
+            const string message = "Are you sure that you would like to logout and exit the SW ?";
+            const string caption = "Termination!";
+            var result = MessageBox.Show(message, caption,
+                               MessageBoxButtons.YesNo,
+                               MessageBoxIcon.Question);
+
+            e.Cancel = (result == DialogResult.No);
+
+        }
+
     }
 }
